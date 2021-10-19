efe6d9a6400db8a4aece9fdb825258657ea11e1b
Yuhei Yamakawa <ymkwyh1@gmail.com>
2021-10-19 17:40:14 +0900

    boardの一覧表示

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  Board.create!(
+    name: Faker::Lorem.sentence(word_count: 3),
+    content: Faker::Lorem.sentence(word_count: 10)
+  )
+end
