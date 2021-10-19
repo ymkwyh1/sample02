e75167464d6981afb1e560993353ac1c3d4fa6cb
Yuhei Yamakawa <ymkwyh1@gmail.com>
2021-10-19 18:13:51 +0900

    board new & board create

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -8,9 +8,18 @@ class BoardsController < ApplicationController
     end
 
     def new
+        @board = Board.new
     end
 
     def create
+        @board = Board.new(board_params)
+        if @board.save
+            redirect_to boards_path, notice: '保存しました'
+        else
+            flash.now[:error] = '保存に失敗しました'
+            render :new
+        end
+        
     end
 
     def edit
@@ -20,6 +29,11 @@ class BoardsController < ApplicationController
     end
 
     def destroy
+    end
+    
+    private
+    def board_params
+        params.require(:board).permit(:name, :content)
     end
     
 end
