32dbe33ae39f0dfa9a9f1b0c9e615ae7d67418b1
Yuhei Yamakawa <ymkwyh1@gmail.com>
2021-10-23 17:19:51 +0900

    保守性の向上 / cssの修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,9 @@
 class TasksController < ApplicationController
 
+    before_action :set_board, only: [:index, :show, :create, :edit, :update]
     before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
     def index
-        @board = Board.find_by(id: params[:board_id])
         @tasks = @board.tasks.all
     end
 
@@ -12,14 +12,12 @@ class TasksController < ApplicationController
     end
 
     def show
-        @board = Board.find_by(id: params[:board_id])
         @task = @board.tasks.find_by(id: params[:id])
         @comments = @task.comments.all
     end
 
     def create
         @task = current_user.tasks.build(task_params)
-        @board = Board.find_by(id: params[:board_id])
         @task.board_id = @board.id
         if @task.save
             redirect_to board_tasks_path(board_id: @board.id), notice: 'Saved!'
@@ -30,12 +28,10 @@ class TasksController < ApplicationController
     end
 
     def edit
-        @board = Board.find_by(id: params[:board_id])
         @task = @board.tasks.find_by(id: params[:id])
     end
 
     def update
-        @board = Board.find_by(id: params[:board_id])
         @task = @board.tasks.find_by(id: params[:id])
         if @task.update(task_params)
           redirect_to board_task_path(board_id: @board.id, id: @task.id), notice: 'Updated!'
@@ -56,6 +52,10 @@ class TasksController < ApplicationController
     private
     def task_params
         params.require(:task).permit(:name, :content, :eyecatch)
+    end
+
+    def set_board
+        @board = Board.find_by(id: params[:board_id])
     end
 
 end
